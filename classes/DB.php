3b1e664ef5b49d0b7552c4093928ac877547feaf
Sien Van den Bergh <sienvdbergh@gmail.com>
2022-03-18 22:27:51 +0100

    feat: make class of DB connection

---
+++ classes/DB.php
@@ -0,0 +1,15 @@
+<?php
+
+    abstract class DB { 
+        private static $conn; 
+
+        public static function getConnection(){
+            if(self::$conn !== null){
+                //connection found
+                return self::$conn;
+        }else{
+            self::$conn = new PDO("mysql:host=localhost;dbname=imdmedia", "root", "");
+            return self::$conn;
+            }
+        }
+    }
