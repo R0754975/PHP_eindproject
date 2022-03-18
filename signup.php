0a8d621983abcbed5d90f9c8995bac73a960e171
Sien Van den Bergh <sienvdbergh@gmail.com>
2022-03-18 11:50:41 +0100

    feat: add html of signup form

---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Signup IMDMedia</title>
+</head>
+<body>
+    <div class="IMDMediaSignIn">
+        <form action="" method="post">
+            <h2>Signup to IMDMedia</h2>
+            <p>Get inspired by your fellow students!</p>
+
+            <div class="form__field">
+                <label for="Username">Username</label>
+                <input autocomplete="off" type="text" name="username">
+            </div>
+
+            <div class="form__field">
+                <label for="Email">Email</label>
+                <input autocomplete="on" type="text" name="email">
+            </div>
+
+            <div class="form__field">
+                <label for="Password">Password</label>
+                <input type="password" name="password">
+            </div>
+
+            <div class="form__field">
+                <label for="Password">Repeat password</label>
+                <input type="password" name="passwordRepeat">
+            </div>
+
+            <div class="form__field">
+                <input type="submit" value="Sign up" class="primarybtn">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
