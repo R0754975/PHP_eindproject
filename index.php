cd758c2c5e5ad4b4cafe9f7ff387fb45d20706e4
Sienvdb <sienvdbergh@gmail.com>
2022-04-24 17:11:02 +0200

    feat: make first version of index.php

--- index.php
+++ index.php
@@ -10,8 +10,33 @@
     <title>IMDMedia</title>
 </head>
 <body>
-    <a href="logout.php" class="navbar__logout">Hi, logout?</a>	
-    <a href="settings.php" class="navbar__logout">Settings</a>	
+    <div class="nav">
+        <div>
+            <a href="./index.php"><img src="./images/computer.png" alt="IMDMedia logo" class="logo"></a>
+        </div>
+        <div>
+            <form action="" method="post" class="searchBar">
+                <div class="form__field">
+                    <input type="text" name="search">
+                </div>
+                <button type="submit">
+                    <img src="./images/computer.png" alt="IMDMedia logo" class="searchIcon"/>
+                </button>
+            </form>
+        </div>
+        <div>
+            <a href="#" class="primarybtn">Upload project</a>
+            <div>
+                <button class="dropbtn"></button>
+                <div class="dropdownContent">
+                    <a href="#">Profile</a>
+                    <a href="./settings.php">Settings</a>
+                    <a href="./logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 	<script type="module" src="main.js"></script>
 
