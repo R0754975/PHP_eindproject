babcbb705435f6b23f6338e30dbd34d1a45fccc9
R0754975 <r0754975@student.thomasmore.be>
2022-04-24 13:32:56 +0200

    added the ability to upload posts and view them in the index.php

--- index.php
+++ index.php
@@ -1,19 +1,42 @@
 <?php
     include_once("bootstrap.php");
-    include_once("functions.inc.php");
+    include_once("inc/functions.inc.php");
         boot();
-        checkAuthorisation();
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php include_once("header.inc.php"); ?>
-    <title>IMDMedia</title>
-</head>
-<body>
-    <a href="logout.php" class="navbar__logout">Hi, logout?</a>	
-    <a href="settings.php" class="navbar__logout">Settings</a>	
+        $auth = checkLoggedIn();
+        
+        $posts = Post::getAll();
 
-	<script type="module" src="main.js"></script>
-
-</body>
-</html>
+        ?><!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>IMDMedia</title>
+            <link rel="stylesheet" href="css/app.css">
+        </head>
+        <body>
+            <?php if($auth == true): ?>
+            <a href="logout.php" class="navbar__logout">Hi, logout?</a>
+            <?php endif ?>
+            <?php if($auth == false): ?>
+            <a href="signup.php" class="navbar__logout">Hi, would you like to register?</a>
+            <?php endif ?>		
+            <section>
+                <div>
+                    <h1>IMDMedia</h1>
+                    <h2>Welcome to IMDMedia</h2>
+                    <a href="project.php">Upload a new project!</a>
+                </div>
+                <?php foreach($posts as $post): ?>
+                <div class="post">
+                    <h3><?php echo htmlspecialchars($post['title']); ?></h3>
+                    <img src="<?php echo $post['filePath']; ?>" alt="<?php echo $post['title']; ?>">
+                    <?php if($auth == true): ?>
+                    <p><?php echo htmlspecialchars($post['userName']); ?></p>
+                    <?php endif ?>
+                </div>
+                <?php endforeach; ?>    
+            </section>
+        </body>
+        </html>
