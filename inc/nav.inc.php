0a730c35bf17dc539409074bb3f4f8cb46acbf6c
Sienvdb <sienvdbergh@gmail.com>
2022-04-30 14:54:05 +0200

    feat: change nav in nav.inc function

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,16 +1,29 @@
 <?php 
- 
-  $username = $_SESSION['username'];
-?>
-
-<nav class="navbar">
-    <a href="index.php" class="logo">IMDMedia</a>
-    <a href="index.php">Home</a>
-    <a href="mylist.php">Profile</a>
-    
-    <form action="" method="get">
-      <input type="text" name="search">
-    </form>
-    
-    <a href="logout.php" class="navbar__logout">Hi <?php echo $username; ?>, logout?</a>
-</nav>
+  $username = $_SESSION['user'];
+?><div class="nav">
+      <div>
+          <a href="./index.php"><img src="./images/computer.png" alt="IMDMedia logo" class="logo"></a>
+      </div>
+      <div>
+          <form action="" method="post" class="searchBar">
+              <div class="form__field">
+                  <input type="text" name="search">
+              </div>
+              <button type="submit">
+                  <img src="https://res.cloudinary.com/dzhrxvqre/image/upload/v1651321618/IMDMedia_Pictures/searchIcon.png" alt="IMDMedia logo" class="searchIcon"/>
+              </button>
+          </form>
+      </div>
+      <div class="navRight">
+          <a href="#" class="primarybtn">Upload project</a>
+          <div class="account">
+              <button class="dropbtn"><img src="https://res.cloudinary.com/dzhrxvqre/image/upload/v1651321618/IMDMedia_Pictures/searchIcon.png" alt="ProfilePic"></button>
+              <div class="dropdownContent"> 
+                  <a href="#">Profile</a>
+                  <a href="./settings.php">Settings</a>
+                  <hr>
+                  <a href="./logout.php">Logout</a>
+              </div>
+          </div>
+      </div>
+  </div>
